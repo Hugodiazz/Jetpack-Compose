33f8e18307228dfe78b0d51d464bb05cb3da9021
Hugodiazz <hugozaid2002@gmail.com>
2025-01-14 14:22:09 -0600

    First commit
    Resources and configurations

---
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.jetpackcompose.instagramUi.LoginScreen
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                LoginScreen()
+            }
+        }
+    }
+}
